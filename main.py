f2d360e75eedaa4679e96ccda1dbf6651667f180
ramneetkang-07 <ramneetkang2000@gmail.com>
2026-02-10 21:42:57 +0530

    Add method to call unary_simplify function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from extraction_element import extraction
 from evaluation import expression_eval
+from unary import unary_simplify
 
 user_input = (input("Enter the input: ")).replace(" ","")
 expression_list = extraction(user_input)
-print(expression_list)
+
+expression_list = unary_simplify(expression_list)
 
 result = ( expression_eval(expression_list) )[0]
 print(result)
